465e414d678da53f38b141e9dccd8cd819ca3fbe
abkour <floralysman@gmail.com>
2022-02-23 13:50:53 +0000

    Added point type

---
+++ impl/point2.cpp
@@ -0,0 +1,13 @@
+#include "point2.hpp"
+
+namespace tinygui {
+
+Point2::Point2() {
+
+}
+
+Point2::Point2(float px, float py) {
+
+}
+
+}
---
+++ impl/point2.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace tinygui {
+
+struct Point2 {
+
+	Point2();
+	Point2(float px, float py);
+
+	float x, y;
+};
+
+}
