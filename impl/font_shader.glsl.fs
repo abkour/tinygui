b311a9de266f6ef8a8d686019e3dee3a4f9e9b89
abkour <floralysman@gmail.com>
2022-03-05 12:20:00 +0000

    fixed issue with font color

--- impl/font_shader.glsl.fs
+++ impl/font_shader.glsl.fs
@@ -4,9 +4,9 @@ layout(location = 0) out vec4 FragmentColor;
 in vec2 texCoord;
 
 uniform sampler2D inputText;
-uniform vec3 FontColor;
+uniform vec3 fontColor;
 
 void main() {
-    vec4 alpha = vec4(1.f, 1.f, 1.f, texture(inputText, texCoord).r);
+    vec4 alpha = vec4(fontColor, texture(inputText, texCoord).r);
     FragmentColor = vec4(1.f) * alpha;
 }
