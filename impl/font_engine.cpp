624a747ebed394a0e76f0525e99da2ad858cbe06
abkour <floralysman@gmail.com>
2022-03-05 06:55:04 +0000

    advanced font engine

--- impl/font_engine.cpp
+++ impl/font_engine.cpp
@@ -1,8 +1,16 @@
 #include "font_engine.hpp"
 
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+
 namespace tinygui {
 
 FontEngine::FontEngine(const char* pathToFont) {
+
+    // OpenGL demands a 4byte alignment on texture elements. Our fonts have 1 byte alignment.
+    // Therefore, we need to set the pixel alignment to 1byte.
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
 	FT_Library ft;
 	if (FT_Init_FreeType(&ft)) {
@@ -39,6 +47,7 @@ FontEngine::FontEngine(const char* pathToFont) {
             GL_UNSIGNED_BYTE,
             face->glyph->bitmap.buffer
         );
+
         // set texture options
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
@@ -49,24 +58,16 @@ FontEngine::FontEngine(const char* pathToFont) {
             texture,
             Point2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
             Point2(face->glyph->bitmap_left, face->glyph->bitmap_top),
-            face->glyph->advance.x
+            face->glyph->advance.x >> 6
         ); 
+     
         characters.insert(std::pair<char, Character>(c, character));
     }
 
     // We no longer need to interface with the FT library.
     FT_Done_Face(face);
     FT_Done_FreeType(ft);
-
-    // OpenGL demands a 4byte alignment on texture elements. Our fonts have 1 byte alignment.
-    // Therefore, we need to set the pixel alignment to 1byte.
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-
-    // To only render the parts of the quad that are occupied by character pixels,
-    // we enable alpha blending. See font_shader.glsl.fs/main for use case.
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
+    
     glGenVertexArrays(1, &quadvao);
     glGenBuffers(1, &quadvbo);
     glBindVertexArray(quadvao);
@@ -78,11 +79,64 @@ FontEngine::FontEngine(const char* pathToFont) {
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), reinterpret_cast<void*>(2 * sizeof(float)));
 
-    
+    glm::mat4 projection = glm::ortho(0.f, 1920.f, 0.f, 1080.f);
+
+    ShaderWrapper tmp_font_shader(
+        false,
+        shader_p(GL_VERTEX_SHADER, "C://Users//flora//source//repos//tinygui//impl//font_shader.glsl.vs"),
+        shader_p(GL_FRAGMENT_SHADER, "C://Users//flora//source//repos//tinygui//impl//font_shader.glsl.fs")
+    );
+    font_shader = std::move(tmp_font_shader);
+    font_shader.bind();
+    glUniformMatrix4fv(glGetUniformLocation(font_shader.id(), "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
+    // To only render the parts of the quad that are occupied by character pixels,
+    // we enable alpha blending. See font_shader.glsl.fs/main for use case.
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+}
+
+FontEngine::~FontEngine() {
+    for (auto& ch : characters) {
+        glDeleteTextures(1, &ch.second.textureID);
+    }
 }
 
 void FontEngine::renderText(const std::string& text) {
+    font_shader.bind();
+    float color[3] = { 1.f, 1.f, 1.f };
+    glUniform3fv(glGetUniformLocation(font_shader.id(), "color"), 1, color);
 
+    std::cout.sync_with_stdio(false);
+
+    glBindVertexArray(quadvao);
+    glBindBuffer(GL_ARRAY_BUFFER, quadvbo);
+    unsigned advance = 0;
+    Point2 vertices[6];
+    for (int i = 0; i < text.size(); ++i) {
+        auto ch = characters.find(text[i]);
+        Point2 bl(advance + ch->second.bearing.x, 200 - (ch->second.size.y - ch->second.bearing.y));
+        Point2 tr(advance + ch->second.bearing.x + ch->second.size.x, 200 + ch->second.bearing.y);
+        advance += ch->second.advance;
+        
+        // We have to flip the UV coordinates. That's why this looks kinda weird on first glance.
+        Point2 vertices[12] = {
+            { bl.x, tr.y },     Point2(0.f, 0.f),
+            tr,                 Point2(1.f, 0.f),
+            { tr.x, bl.y },     Point2(1.f, 1.f),
+            
+            { bl.x, tr.y },     Point2(0.f, 0.f),
+            bl,                 Point2(0.f, 1.f),
+            { tr.x, bl.y },     Point2(1.f, 1.f)
+        };
+        
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+
+        glBindTexture(GL_TEXTURE_2D, ch->second.textureID);
+        glActiveTexture(GL_TEXTURE0);
+        
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+    }
 }
 
 
