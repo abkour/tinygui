2013a4f4d80ce4780eb7d32d3ac9e11f7a544b6a
abkour <floralysman@gmail.com>
2022-03-05 06:55:31 +0000

    changed gui shader

--- impl/gui_shader.glsl.fs
+++ impl/gui_shader.glsl.fs
@@ -9,31 +9,15 @@ layout(std430, binding = 0) buffer ssbo
 
 flat in int rect_id;
 
-const vec3 colors[4] = vec3[4]
-(
-    vec3(1.f, 0.f, 0.f),
-    vec3(0.f, 1.f, 0.f),
-    vec3(0.f, 0.f, 1.f),
-    vec3(1.f, 1.f, 0.f) // If cursor hovers over rectangle we change color to yellow
-);
+#include <C:\Users\flora\source\repos\tinygui\impl\colortable.data>
+#include <C:\Users\flora\source\repos\tinygui\impl\extended.glsl.vs>
 
 uniform float cursorX;
 uniform float cursorY;
 
-//#include <C:\Users\flora\source\repos\tinygui\impl\extended.glsl.vs>
-
-bool compareCoordinates(vec2 cursor) {
-    return cursor + vec2(0.5) == gl_FragCoord.xy;
-}
-
 void main() {
-   
     if(compareCoordinates(vec2(cursorX, cursorY))) {
         idOfRectangle = rect_id;
     }
-    /*
-    if(vec2(cursorX, cursorY) + vec2(0.5) == gl_FragCoord.xy) {
-         idOfRectangle = rect_id;
-    }*/
     FragmentColor = vec4(colors[rect_id], 1.f);
 }
