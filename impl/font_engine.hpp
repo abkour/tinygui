25f802045174b413de3526c1c09aa24c9f7f3708
abkour <floralysman@gmail.com>
2022-03-05 06:55:12 +0000

    advanced font engine

--- impl/font_engine.hpp
+++ impl/font_engine.hpp
@@ -3,38 +3,43 @@
 #include FT_FREETYPE_H
 #include <glad/glad.h>
 
-#include <glshader.hpp>
+#include <shaderdirect.hpp>
 
 #include <iostream>
 #include <map>
 #include <string>
 
 #include "point2.hpp"
-
+#include <mat4.hpp>
+#include <shaderdirect.hpp>
 
 namespace tinygui {
 
 struct Character {
 
-	Character(GLuint pTextureID, Point2 pQuadmin, Point2 pQuadmax, uint32_t pAdvance)  
+	Character(GLuint pTextureID, Point2 pSize, Point2 pBearing, uint32_t pAdvance)  
 		: textureID(pTextureID)
-		, quadmin(pQuadmin)
-		, quadmax(pQuadmax)
+		, size(pSize)
+		, bearing(pBearing)
 		, advance(pAdvance)
 	{}
 
 	GLuint textureID;
-	Point2 quadmin, quadmax;
+	Point2 size, bearing;
 	uint32_t advance;
 };
 
 struct FontEngine {
 
-	FontEngine(const char* pathToFont = "../rsc/consolas.fft");
+	FontEngine(const char* pathToFont = "C://Users//flora//source//repos//tinygui//rsc//consola.ttf");
+
+	~FontEngine();
 
 	void renderText(const std::string& text);
 
 private:
+
+	ShaderWrapper font_shader;
 
 	std::map<char, Character> characters;
 	GLuint quadvao, quadvbo;
